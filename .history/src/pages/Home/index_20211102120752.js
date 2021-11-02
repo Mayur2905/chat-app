abb1e034fd280e8e4c562996006d2ad0d8aa6e1f
Mayur2905 <mayurgaikwad0101@gmail.com>
2021-11-02 13:46:49 +0530

    created for home and created chat page

---
+++ .history/src/pages/Home/index_20211102120752.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/named */
+import React from 'react'
+import { Switch,Route } from 'react-router';
+import { Col, Grid, Row } from 'rsuite';
+import Sidebar from '../../components/Sidebar'
+import { RoomsProvider } from '../../context/rooms.context';
+import Chat from './Chat';
+
+const Home = () => {
+    return(
+        <RoomsProvider>
+        <Grid fluid className='h-100'>
+            <Row className='h-100'>
+                <Col xs={24} md={8} className='h-100'>
+                    <Sidebar />
+                </Col>
+                <Switch>
+                    <Route exact path="/chat/:chatId">
+                        <Col xs={24} md={16} className="h-100">
+                         <Chat/>
+                        </Col>
+                    </Route>
+                </Switch>
+            </Row>
+        </Grid>
+        </RoomsProvider>
+    );
+};
+
+export default Home;
---
+++ src/pages/Home/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chat = () => {
+    return (
+        <div>hello </div>
+    )
+}
+
+export default Chat;
